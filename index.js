2c628045ffbf63b3a26314326bfef334752d9016
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2022-10-25 05:44:28 +0600

    server created

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000 ;
+
+app.use(cors())
+
+const categories =require('./data/categories.json');
+const news = require('./data/news.json');
+
+app.get('/', (req, res)=>{
+    res.send('new api running');
+});
