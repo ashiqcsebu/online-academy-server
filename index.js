72d11ce7806993c0caefe175c523f689fb8dae4b
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2022-10-25 06:16:41 +0600

    logic set

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express();
-const cors = require('cors')
+// const cors = require('cors')
 const port = process.env.PORT || 5000 ;
 
-app.use(cors())
+// app.use(cors())
 
-const categories =require('./data/categories.json');
-const news = require('./data/news.json');
+const courses =require('./data/courses.json');
+ const courseInfo = require('./data/courseInfo.json');
 
-app.get('/', (req, res)=>{
-    res.send('new api running');
+
+app.get('/courses', (req, res)=>{
+   res.send(courseInfo);
+ });
+
+app.get('/course-categories', (req, res)=>{
+    res.send(courses);
 });
+
+app.get('/course-info/:id' , (req , res)=>{
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(courseInfo)
+    }
+    const category_news = courseInfo.filter(n => n.category_id === id);
+    res.send(category_news);
+});
+
+// app.get('/course/:id' , (req , res)=>{
+
+//     const id = req.params.id ;
+//     const selectedNews = courseInfo.find(n => n._id === id);
+//     res.send(selectedNews);
+// });
+
+app.listen(port, () => {
+   console.log('Server running on port', port) ;
+});
+
