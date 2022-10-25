9221a1832f8834a043a68f178509136b5dd1bd37
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2022-10-26 04:28:36 +0600

    index file updated

--- index.js
+++ index.js
@@ -13,25 +13,15 @@ app.get('/courses', (req, res)=>{
    res.send(courses);
  });
 
-app.get('/course-categories', (req, res)=>{
-    res.send(coursesInfo);
+ app.get('/course-categories', (req, res)=>{
+    res.send(courseInfo);
 });
 
-app.get('/course-info/:id' , (req , res)=>{
+app.get('/courses/:id' , (req , res)=>{
     const id = req.params.id;
-    if(id === '08'){
-        res.send(courseInfo)
-    }
-    const category_news = courseInfo.filter(n => n.category_id === id);
-    res.send(category_news);
+    const course_details = courseInfo.find(course_detail => course_detail.category_id === id);
+    res.send(course_details);
 });
-
-// app.get('/course/:id' , (req , res)=>{
-
-//     const id = req.params.id ;
-//     const selectedNews = courseInfo.find(n => n._id === id);
-//     res.send(selectedNews);
-// });
 
 app.listen(port, () => {
    console.log('Server running on port', port) ;
